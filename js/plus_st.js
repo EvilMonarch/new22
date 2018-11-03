a4dab170366a171c4794db08b7abbf2ffc4fcdc9
EvilMonarch <59070016@it.kmitl.ac.th>
2018-11-03 07:19:00 +0700

    up
    up

---
+++ js/plus_st.js
@@ -0,0 +1,58 @@
+if(localStorage.getItem('id')==""){
+    window.location.href="login.html";
+  }
+const form = document.querySelector('#add');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    var passcourse = localStorage.getItem("passcourse")
+    var namecourse = localStorage.getItem("namecourse")
+    var nameteacher = localStorage.getItem("nameteacher")
+    var day = localStorage.getItem("day")
+    var time = localStorage.getItem("time")
+    var room = localStorage.getItem("room")
+    var std = document.getElementById("std").value
+    var teacher = localStorage.getItem("id")
+    db.collection('students').doc(std).collection('subject').doc(namecourse).set({
+
+        passcourse: passcourse,
+        namecourse: namecourse,
+        nameteacher: nameteacher,
+        day: day,
+        time: time,
+        teacher: teacher,
+        room: room
+
+
+
+    });
+    form.std.value = '';
+
+
+    var subject = firebase.firestore();
+
+    subject.collection('teacher').doc(localStorage.getItem('id')).collection('subject').doc(localStorage.getItem("namecourse")).get()
+    .then(function (doc){
+
+        let dt = []
+        dt = doc.data().student
+        dt.push(std)
+
+        test(dt)
+
+    });
+
+
+})
+function test(dt) {
+    
+    db.collection('teacher').doc(localStorage.getItem('id')).collection('subject').doc(localStorage.getItem("namecourse")).update({
+        student: dt
+    })
+    alert("เพิ่มนักศึกษาเรียบร้อย");
+    window.location.href = "blog1.html";
+}
+
+
+
